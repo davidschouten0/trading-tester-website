2ba3ee4dfe35e24a23776e4d153bdf93a6e8e25d
davidschouten0 <davidschouten06@gmail.com>
2026-03-10 14:53:02 +0100

    added Ticker Lookup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flask
 import yfinance as yf
 from backtesting import Backtest
+import requests
 
 from utility.stratSwitcher import stratSwitcher
 
@@ -10,9 +11,9 @@ app = flask.Flask(__name__)
 @app.route("/")
 def index():
     return flask.render_template("index.html")
-    return yf.Lookup(query=flask.request.form.get("ticker")).get_stock(
-        count=5
-    )  # wenn du ticker lookup willst
+    #return yf.Lookup(query=flask.request.form.get("ticker")).get_stock(
+    #    count=5
+    #)  wenn du ticker lookup willst
 
 
 @app.route("/backtest", methods=["POST"])
@@ -38,6 +39,27 @@ def backtest():
         "backtest.html", ticker_html=ticker, strategy_html=strategy, data_html=data
     )
 
+@app.route("/search_ticker")
+def search_ticker():
+    query = flask.request.args.get("q", "")
+    
+    #if there is no query, return an empty JSON
+    if len(query) < 1:
+        return flask.jsonify([])
+    
+    url = f"https://query2.finance.yahoo.com/v1/finance/search?q={query}"
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers) #goes to the URL and gets the data
+    data = response.json() 
+
+    valid_tickers = []
+    if "quotes" in data:
+        for quote in data["quotes"]:
+            if "symbol" in quote and (quote["quoteType"] == "EQUITY" or quote["quoteType"] == "ETF"):
+                valid_tickers.append(quote["symbol"]) 
+
+    return flask.jsonify(valid_tickers)
 
 if __name__ == "__main__":
     app.run(debug=True)
