4e49e4ce0350f3e5ac5435e5dc611e805d66e913
D. Kern <hottorun@pm.me>
2026-03-10 17:37:10 +0100

    more strategies

---
+++ utility/test.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+from backtesting import Backtest
+
+import strategies.williamsStrat as rev
+
+historic_data = yf.download("AAPL", period="1y", interval="1h")
+historic_data = (
+    historic_data.droplevel("Ticker", axis=1).reset_index().set_index("Datetime")
+)
+historic_data = historic_data[["Open", "High", "Low", "Close", "Volume"]]
+
+bt = Backtest(historic_data, rev.WilliamsStrategy, cash=100000, commission=(0.2, 0))
+results = bt.run()
+print(results)
+bt.plot()
